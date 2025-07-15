dd40def0e1ec9db6f48cc55ccf3659627fbd9dd5
unknown <yamilsaad.infraestructura@gmail.com>
2025-07-15 11:05:49 -0300

    nueva correccion de mp3

--- lib/src/presentation/screens/splash_screen/screen_splash.dart
+++ lib/src/presentation/screens/splash_screen/screen_splash.dart
@@ -1,10 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:get/get.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(milliseconds: 3000), () {
+      // Navegar al login después del splash
+      Get.offAllNamed('/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/logo.png',
+          width: 180,
+          height: 180,
+        ).animate().fadeIn(duration: 3000.ms),
+      ),
+    );
+  }
+}
+
+
+
 //El siguiente código es un ejemplo de una pantalla de splash en
 //Flutter que muestra un logo y navega a la pantalla de login
 //después de 3 segundos. Utiliza el paquete `audioplayer´ para integrearle
 //un sonido de intro welcome.mp3.
 //Se necesita agregar el paquete `audioplayers` en el archivo `pubspec.yaml`
 //y también agregar el archivo de sonido en la carpeta `assets/sounds/`.
-
+/*
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:get/get.dart';
@@ -55,40 +93,5 @@ class _SplashScreenState extends State<SplashScreen> {
       ),
     );
   }
-}
-
-/*import 'package:flutter/material.dart';
-import 'package:flutter_animate/flutter_animate.dart';
-import 'package:get/get.dart';
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(milliseconds: 3000), () {
-      // Navegar al login después del splash
-      Get.offAllNamed('/login');
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Image.asset(
-          'assets/images/logo.png',
-          width: 180,
-          height: 180,
-        ).animate().fadeIn(duration: 3000.ms),
-      ),
-    );
-  }
 }*/
+
